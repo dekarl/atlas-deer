bf975c34fb97a150aa127d5cb7214e1a1b65261a
Fred van den Driessche <fred@metabroadcast.com>
2014-01-10 17:54:01 +0000

    Only write new Broadcasts of Content from bootstrap stream

--- atlas-processing/src/main/java/org/atlasapi/system/bootstrap/workers/BootstrapContentPersistor.java
+++ atlas-processing/src/main/java/org/atlasapi/system/bootstrap/workers/BootstrapContentPersistor.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.atlasapi.content.Broadcast;
 import org.atlasapi.content.Container;
 import org.atlasapi.content.Content;
+import org.atlasapi.content.ContentStore;
 import org.atlasapi.content.ContentVisitorAdapter;
 import org.atlasapi.content.ContentWriter;
 import org.atlasapi.content.Item;
 import org.atlasapi.content.ItemAndBroadcast;
 import org.atlasapi.content.Version;
+import org.atlasapi.entity.util.Resolved;
 import org.atlasapi.entity.util.RuntimeWriteException;
 import org.atlasapi.entity.util.WriteException;
 import org.atlasapi.entity.util.WriteResult;
@@ -21,21 +23,26 @@ import org.joda.time.Interval;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Function;
+import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
 import com.metabroadcast.common.base.Maybe;
 
 public class BootstrapContentPersistor implements ContentWriter {
     
     private final Logger log = LoggerFactory.getLogger(getClass());
 
-    private final ContentWriter contentWriter;
+    private final ContentStore contentStore;
     private final ScheduleWriter scheduleWriter;
     private final ChannelResolver channelResolver;
     private final PersistingContentVisitor visitor = new PersistingContentVisitor();
     
-    public BootstrapContentPersistor(ContentWriter contentWriter, ScheduleWriter scheduleWriter,
+    public BootstrapContentPersistor(ContentStore contentStore, ScheduleWriter scheduleWriter,
             ChannelResolver channelResolver) {
-        this.contentWriter = contentWriter;
+        this.contentStore = contentStore;
         this.scheduleWriter = scheduleWriter;
         this.channelResolver = channelResolver;
     }
@@ -49,7 +56,14 @@ public class BootstrapContentPersistor implements ContentWriter {
 
         @Override
         protected WriteResult<? extends Content> visitItem(Item item) {
-            WriteResult<? extends Content> result = writeBroadcasts(item);
+            WriteResult<? extends Content> result = null;
+            if (hasBroadcasts(item)) {
+                try {
+                    result = writeNewBroadcasts(item);
+                } catch (WriteException e) {
+                    throw new RuntimeWriteException(e);
+                }
+            }
             if (result == null || !result.written()) {
                 log.debug("bootstrapping {}", item);
                 result = writeContent(item);
@@ -57,11 +71,20 @@ public class BootstrapContentPersistor implements ContentWriter {
             return result;
         }
 
-        private WriteResult<? extends Content> writeBroadcasts(Item item) {
+        private boolean hasBroadcasts(Item item) {
+            for (Version version : item.getVersions()) {
+                if (!version.getBroadcasts().isEmpty()) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private WriteResult<? extends Content> writeNewBroadcasts(Item item, Optional<Content> current) {
             WriteResult<? extends Content> result = null;
             for (Version version : item.getVersions()) {
                 for (Broadcast broadcast : version.getBroadcasts()) {
-                    if (broadcast.getSourceId() != null) {
+                    if (broadcast.getSourceId() != null && !hasBroadcast(current, broadcast)) {
                         try {
                             ItemAndBroadcast iab = new ItemAndBroadcast(item, broadcast);
                             log.debug("bootstrapping {}", iab);
@@ -75,9 +98,39 @@ public class BootstrapContentPersistor implements ContentWriter {
             return result;
         }
         
+        private boolean hasBroadcast(Optional<Content> current, Broadcast broadcast) {
+            if (current.isPresent()) {
+                return false;
+            }
+            Content currentContent = current.get();
+            if (currentContent instanceof Item) {
+                Item currentItem = (Item) currentContent;
+                for (Version version : currentItem.getVersions()) {
+                    if (version.getBroadcasts().contains(broadcast)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private WriteResult<? extends Content> writeNewBroadcasts(final Item item) throws WriteException {
+            ListenableFuture<Resolved<Content>> resolved = contentStore.resolveIds(ImmutableList.of(item.getId()));
+            ListenableFuture<WriteResult<? extends Content>> result = Futures.transform(resolved, 
+                new Function<Resolved<Content>, WriteResult<? extends Content>>() {
+                    @Override
+                    public WriteResult<? extends Content> apply(Resolved<Content> input) {
+                        Optional<Content> current = input.toMap().get(item.getId());
+                        return writeNewBroadcasts(item, current);
+                    }
+                }
+            );
+            return Futures.get(result, WriteException.class);
+        }
+
         private <C extends Content> WriteResult<C> writeContent(C content) {
             try {
-                return contentWriter.writeContent(content);
+                return contentStore.writeContent(content);
             } catch (WriteException e) {
                 throw new RuntimeWriteException(e);
             }
