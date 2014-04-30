5fc3028a2ae2c09a970c91bcd46fae401e3e9927
Fred van den Driessche <fred@metabroadcast.com>
2014-04-30 10:29:44 +0100

    Default to quorum consistency reads on processing

--- atlas-cassandra/src/main/java/org/atlasapi/CassandraPersistenceModule.java
+++ atlas-cassandra/src/main/java/org/atlasapi/CassandraPersistenceModule.java
@@ -18,9 +18,11 @@ import org.atlasapi.topic.Topic;
 
 import com.datastax.driver.core.Session;
 import com.google.common.base.Equivalence;
+import com.google.common.base.Objects;
 import com.google.common.util.concurrent.AbstractIdleService;
 import com.metabroadcast.common.ids.IdGeneratorBuilder;
 import com.metabroadcast.common.properties.Configurer;
+import com.metabroadcast.common.properties.Parameter;
 import com.metabroadcast.common.queue.Message;
 import com.metabroadcast.common.queue.MessageSender;
 import com.metabroadcast.common.queue.MessageSenderFactory;
@@ -35,6 +37,8 @@ public class CassandraPersistenceModule extends AbstractIdleService implements P
     private String contentChanges = Configurer.get("messaging.destination.content.changes").get();
     private String topicChanges = Configurer.get("messaging.destination.topics.changes").get();
     private String scheduleChanges = Configurer.get("messaging.destination.schedule.changes").get();
+    
+    private Boolean processing = Objects.firstNonNull(Configurer.get("processing.config"), Parameter.valueOf("false")).toBoolean();
     
     private final String keyspace;
 
@@ -56,18 +60,19 @@ public class CassandraPersistenceModule extends AbstractIdleService implements P
         this.messageSenderFactory = messageSenderFactory;
         this.keyspace = keyspace;
         this.context = context;
+        ConsistencyLevel readConsistency = processing ? ConsistencyLevel.CL_QUORUM : ConsistencyLevel.CL_ONE;
         this.contentStore = CassandraContentStore.builder(context, "content", 
             hasher, sender(contentChanges, ResourceUpdatedMessage.class), idGeneratorBuilder.generator("content"))
-            .withReadConsistency(ConsistencyLevel.CL_ONE)
+            .withReadConsistency(readConsistency)
             .withWriteConsistency(ConsistencyLevel.CL_QUORUM)
             .build();
         this.topicStore = CassandraTopicStore.builder(context, "topics", 
             topicEquivalence(), sender(topicChanges, ResourceUpdatedMessage.class), idGeneratorBuilder.generator("topic"))
-            .withReadConsistency(ConsistencyLevel.CL_ONE)
+            .withReadConsistency(readConsistency)
             .withWriteConsistency(ConsistencyLevel.CL_QUORUM)
             .build();
         this.scheduleStore = CassandraScheduleStore.builder(context, "schedule", contentStore, sender(scheduleChanges, ScheduleUpdateMessage.class))
-                .withReadConsistency(ConsistencyLevel.CL_ONE)
+                .withReadConsistency(readConsistency)
                 .withWriteConsistency(ConsistencyLevel.CL_QUORUM)
                 .build();
         this.dataStaxService = datastaxCassandraService;
@@ -77,7 +82,8 @@ public class CassandraPersistenceModule extends AbstractIdleService implements P
     protected void startUp() throws Exception {
         dataStaxService.awaitRunning();
         Session session = dataStaxService.getSession(keyspace);
-        com.datastax.driver.core.ConsistencyLevel read = com.datastax.driver.core.ConsistencyLevel.ONE;
+        com.datastax.driver.core.ConsistencyLevel read = processing ? com.datastax.driver.core.ConsistencyLevel.QUORUM
+                                                                    : com.datastax.driver.core.ConsistencyLevel.ONE;
         com.datastax.driver.core.ConsistencyLevel write = com.datastax.driver.core.ConsistencyLevel.QUORUM;
         this.contentEquivalenceGraphStore = new CassandraEquivalenceGraphStore(sender(contentEquivalenceGraphChanges, EquivalenceGraphUpdateMessage.class), session, read, write);
         this.equivalentContentStore = new CassandraEquivalentContentStore(contentStore, contentEquivalenceGraphStore, session, read, write);
