0db9f8ebdfa7627d052a25d70de0280342421807
Fred van den Driessche <fred@metabroadcast.com>
2014-05-01 22:22:10 +0100

    schedule comparison script

--- atlas-api/src/test/resources/schedule-compare.py
+++ atlas-api/src/test/resources/schedule-compare.py
@@ -6,73 +6,151 @@ import datetime
 import httplib
 import sys
 import json
+from tabulate import tabulate
 
 class Struct:
-    def __init__(self, **entries): 
-        self.__dict__.update(entries)
+  def __init__(self, **entries):
+    self.__dict__.update(entries)
+
+class SimpleEntry:
+  def __init__(self, id, title, bid, start, end):
+    self.id = id
+    self.title = title
+    self.bid = bid
+    self.start = start
+    self.end = end
+
+  def __str__(self):
+    return "%s %s %s %s %s" % (self.id,self.title,self.bid,self.start,self.end)
+
+  def __repr__(self):
+    return self.__str__()
+
+  def as_list(self):
+    fmt = "%H:%M:%S"
+    start = self.start.strftime(fmt)
+    end = self.end.strftime(fmt)
+    return [start, end, self.bid, self.id, self.title[:15]]
 
 class Atlas:
+  def __init__(self, host, port, version, key):
+    self.host = host
+    self.port = port
+    self.version = version
+    self.key = key
 
-	def __init__(self, host, port, version, key):
-		self.host = host
-		self.port = port
-		self.version = version
-		self.key = key
+  def get_schedule(self, source, channel, start, end):
+    resource = self.get_resource(source, channel, start, end)
+    response = self.get(resource)
+    return self.simplify(response)
 
-	def get_schedule(self, source, channel, start, end):
-		resource = self.get_resource(source, channel, start, end)
-		response = self.get(resource)
-		return self.simplify(response)
+  def get(self, resource):
+    conn = httplib.HTTPConnection(self.host, self.port)
+    conn.request('GET', resource)
+    color = "32" if self.version == 3 else "33"
+    print "\x1b[%smGET http://%s:%s%s\x1b[0m" % (color, self.host, self.port, resource)
+    resp = conn.getresponse()
+    if not resp.status == 200:
+      if resp.status == 400:
+        print "request failed for %s: %s" % (resource, resp.reason)
+      if resp.status == 404:
+        print "resource %s doesn't appear to exist" % (resource)
+      if resp.status >= 500:
+        print "problem with %s? %s %s" % (self.host, resp.status, resp.reason)
+      resp.read()
+      conn.close()
+      sys.exit()
+    return Struct(**json.loads(resp.read()))
 
-	def get(self, resource):
-		conn = httplib.HTTPConnection(self.host, self.port)
-		conn.request('GET', resource)
-		print "GET http://%s:%s%s" % (self.host, self.port, resource)
-		resp = conn.getresponse()
-		if not resp.status == 200:
-			if resp.status == 400:
-				print "request failed for %s: %s" % (resource, resp.reason)
-			if resp.status == 404:
-				print "resource %s doesn't appear to exist" % (resource)
-			if resp.status >= 500:
-				print "problem with %s? %s %s" % (self.host, resp.status, resp.reason)
-			resp.read()
-			conn.close()
-			sys.exit()
-		return Struct(**json.loads(resp.read()))
+  def v4_channel_id(self, channel):
+    prefix = 'http://atlas.metabroadcast.com/4.0/channels/'
+    for a in channel.aliases:
+      if a.startswith(prefix):
+        return a[len(prefix):]
+    raise Exception("couldn't find v4 channel id for %s" % channel.id)
 
-	def simplify(self,response):
-		return response
+  def join(self, params):
+    return "&".join(["%s=%s"%(k,v) for k,v in params.iteritems() if not v == None])
 
-	def v4_channel_id(self, channel):
-		prefix = 'http://atlas.metabroadcast.com/4.0/channels/'
-		for a in channel.aliases:
-			if a.startswith(prefix):
-				return a[len(prefix):]
-		raise Exception("couldn't find v4 channel id for %s" % channel.id)
+  def get_resource(self, source, channel, start, end):
+    if (self.version == 3):
+      params = {
+        'publisher':source,
+        'channel_id' : channel.id,
+        'from':start.isoformat(),
+        'to':end.isoformat(),
+        'apiKey':self.key
+      }
+      return "/3.0/schedule.json?%s" % self.join(params)
+    if (self.version == 4):
+      params = {'source':source,'from':start.isoformat(),'to':end.isoformat(),'apiKey':self.key}
+      channel_id = self.v4_channel_id(channel)
+      return "/4.0/schedules/%s.json?annotations=content.description&%s" %(channel_id, self.join(params))
+    raise Exception("unexpected version %s" % self.version)
 
-	def join(self, params):
-		return "&".join(["%s=%s"%(k,v) for k,v in params.iteritems() if not v == None])
+  def simplify(self, schedule):
+    if self.version == 4:
+      return self.simplify_v4(schedule)
+    return self.simplify_v3(schedule)
 
-	def get_resource(self, source, channel, start, end):
-		if (self.version == 3):
-			params = {
-				'publisher':source, 
-				'channel_id' : channel.id, 
-				'from':start.isoformat(),
-				'to':end.isoformat(),
-				'apiKey':self.key
-			}
-			return "/3.0/schedule.json?%s" % self.join(params)
-		if (self.version == 4):
-			params = {'source':source,'from':start.isoformat(),'to':end.isoformat(),'apiKey':self.key}
-			channel_id = self.v4_channel_id(channel)
-			return "/4.0/schedules/%s.json?%s" %(channel_id, self.join(params))
-		raise Exception("unexpected version %s" % self.version)
+  def simplify_v4(self, schedule):
+    entries = []
+    sched_entries = schedule.schedule['entries'];
+    for sched_entry in sched_entries:
+      i = sched_entry['item']
+      b = sched_entry['broadcast']
+      start = dateutil.parser.parse(b['transmission_time'])
+      end = dateutil.parser.parse(b['transmission_end_time'])
+      entries.append(SimpleEntry(i['id'],i['title'],b['id'],start, end))
+    return entries
 
+  def simplify_v3(self, schedule):
+    entries = []
+    sched_entries = schedule.schedule[0]['items']
+    for se in sched_entries:
+      b = se['broadcasts'][0]
+      start = dateutil.parser.parse(b['transmission_time'])
+      end = dateutil.parser.parse(b['transmission_end_time'])
+      entries.append(SimpleEntry(se['id'],se['title'],b['id'],start,end))
+    return entries
+
+headers = ['Title', 'ID', 'BID', 'Start', 'End']
 
 def compare(left, right):
-	print "comparing\n%s\n%s" % (left, right)
+  table = [headers + ["|"] + headers[::-1]]
+  longer = max(len(left),len(right))
+  l = None
+  r = None
+  while len(left) > 0 and len(right) > 0:
+    l = l if l != None else left.pop()
+    r = r if r != None else right.pop()
+    if (l.start < r.start):
+      table.append(mismatch(None, r))
+      r = None
+    if (l.start > r.start):
+      table.append(mismatch(l, None))
+      l = None
+    else:
+      table.append(matching_start(l, r))
+      l = None
+      r = None
+  if len(left) > 0:
+    for l in left:
+      table.append(mismatch(l, None))
+  else :
+    for r in right:
+      table.append(mismatch(None, r))
+  print tabulate(table, headers="firstrow")
+
+def matching_start(l, r):
+  return l.as_list()[::-1] +["|"]+ r.as_list()
+
+red = lambda x : "\x1b[41m%s\x1b[0m" % (' ' * x)
+missing_row = [red(8),red(8), red(11), red(6), red(15)]
+
+def mismatch(l, r):
+  vals_or_missing = lambda e: missing_row if e == None else e.as_list()
+  return vals_or_missing(l)[::-1] +["|"]+ vals_or_missing(r)
 
 arg_parser = argparse.ArgumentParser(description='Compare Atlas schedules.')
 arg_parser.add_argument('-h1', dest='host1', default='atlas.metabroadcast.com', metavar='host1', help='Remote host for 1st schedule')
@@ -93,11 +171,11 @@ args = arg_parser.parse_args();
 args.start = dateutil.parser.parse(args.start)
 args.end = dateutil.parser.parse(args.end) if not args.end==None else (args.start + datetime.timedelta(days=1))
 
-print "identifying channel %s" % args.channel
+print "identifying channel '%s'" % args.channel
 
 ##
-# Currently there's only a v3 /channels resource so we may need to find the v4 id and we may as well 
-# check that the channel actually exists while were at it. 
+# Currently there's only a v3 /channels resource so we may need to find the v4 id and we may as well
+# check that the channel actually exists while were at it.
 # There's no guarantee that the hosts specified in the args have a 3.0/channels resource so this is
 # hard-coded.
 ##
@@ -105,18 +183,18 @@ channelHost = 'atlas.metabroadcast.com'
 channelResource = '/3.0/channels/%s.json' % args.channel
 channelConn = httplib.HTTPConnection(channelHost)
 channelConn.request('GET', channelResource)
-print "GET http://%s%s" % (channelHost, channelResource)
+print "\x1b[35mGET http://%s%s\x1b[0m" % (channelHost, channelResource)
 channelResp = channelConn.getresponse()
 if not channelResp.status == 200:
-	if channelResp.status == 400:
-		print "request failed for %s: %s" % (args.channel, channelResp.reason)
-	if channelResp.status == 404:
-		print "channel %s doesn't appear to exist" % (args.channel)
-	if channelResp.status >= 500:
-		print "problem with %s? %s %s" % (channelHost, channelResp.status, channelResp.reason)
-	channelResp.read()
-	channelConn.close()
-	sys.exit()
+  if channelResp.status == 400:
+    print "request failed for %s: %s" % (args.channel, channelResp.reason)
+  if channelResp.status == 404:
+    print "channel %s doesn't appear to exist" % (args.channel)
+  if channelResp.status >= 500:
+    print "problem with %s? %s %s" % (channelHost, channelResp.status, channelResp.reason)
+  channelResp.read()
+  channelConn.close()
+  sys.exit()
 
 channel = Struct(**json.loads(channelResp.read())['channels'][0])
 
