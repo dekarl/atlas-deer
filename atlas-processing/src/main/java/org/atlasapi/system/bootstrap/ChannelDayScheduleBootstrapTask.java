b0f3d3ef6e719adb3a1484c39cb04bc6bece70bf
Fred van den Driessche <fred@metabroadcast.com>
2014-05-02 18:03:41 +0100

    bootstrap zero-duration broadcasts

--- atlas-processing/src/main/java/org/atlasapi/system/bootstrap/ChannelDayScheduleBootstrapTask.java
+++ atlas-processing/src/main/java/org/atlasapi/system/bootstrap/ChannelDayScheduleBootstrapTask.java
@@ -17,6 +17,7 @@ import org.atlasapi.content.Episode;
 import org.atlasapi.content.Item;
 import org.atlasapi.content.ItemAndBroadcast;
 import org.atlasapi.entity.Id;
+import org.atlasapi.entity.Identifiables;
 import org.atlasapi.entity.util.ResolveException;
 import org.atlasapi.entity.util.Resolved;
 import org.atlasapi.entity.util.StoreException;
@@ -29,15 +30,20 @@ import org.atlasapi.schedule.ScheduleHierarchy;
 import org.atlasapi.schedule.ScheduleResolver;
 import org.atlasapi.schedule.ScheduleWriter;
 import org.elasticsearch.common.base.Objects;
+import org.joda.time.DateTime;
 import org.joda.time.Interval;
 import org.joda.time.LocalDate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Functions;
 import com.google.common.base.Optional;
+import com.google.common.base.Predicates;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
@@ -97,11 +103,53 @@ public class ChannelDayScheduleBootstrapTask implements Callable<UpdateProgress>
         Map<Id, Optional<Content>> scheduleItems = resolveItems(channelSchedule);
         ensureContainers(Optional.presentInstances(scheduleItems.values()));
         UpdateProgress progress = UpdateProgress.START;
+        //used to handle zero duration broadcasts, assumes no adjacent zero duration broadcasts
+        ItemAndBroadcast prev = null;
         for (ItemAndBroadcast iab : channelSchedule.getEntries()) {
-            Optional<Content> item = scheduleItems.get(iab.getItem().getId());
-            progress = progress.reduce(tryWriteItem(item, iab.getBroadcast(), channel));
+            if (iab.getBroadcast().getBroadcastDuration().getMillis() == 0) {
+                prev = iab;
+            } else {
+                ImmutableList<ItemAndBroadcast> iabs = prev == null ? ImmutableList.of(iab)
+                                                                    : ImmutableList.of(iab, prev);
+                progress = progress.reduce(tryWrite(scheduleItems, iabs, channel));
+                prev = null;
+            }
         }
         return progress;
+    }
+
+    private UpdateProgress tryWrite(Map<Id, Optional<Content>> scheduleItems, 
+            ImmutableList<ItemAndBroadcast> iabs, Channel channel) throws WriteException {
+        
+        List<Id> ids = Lists.transform(iabs, Functions.compose(Identifiables.toId(), ItemAndBroadcast.toItem()));
+        Map<Id, Optional<Content>> items = Maps.filterKeys(scheduleItems, Predicates.in(ids));
+        
+        DateTime earliestStart = null;
+        DateTime latestEnd = null;
+        
+        ImmutableList.Builder<ScheduleHierarchy> schedule = ImmutableList.builder();
+        for (ItemAndBroadcast iab : iabs) {
+            Broadcast broadcast = iab.getBroadcast();
+            Optional<Content> possItem = items.get(iab.getItem().getId());
+            if (!possItem.isPresent()) {
+                log.warn("content not resolved for broadcast " + broadcast);
+                return new UpdateProgress(0, ids.size());
+            }
+            DateTime bcastStart = broadcast.getTransmissionTime();
+            DateTime bcastEnd = broadcast.getTransmissionEndTime();
+            earliestStart = earliestStart == null || earliestStart.isAfter(bcastStart) ? bcastStart
+                                                                                       : earliestStart;
+            latestEnd = latestEnd == null || latestEnd.isBefore(bcastEnd) ? bcastEnd
+                                                                          : latestEnd;
+            schedule.add(ScheduleHierarchy.itemOnly(iab));
+        }
+        try {
+            scheduleWriter.writeSchedule(schedule.build(), channel, new Interval(earliestStart, latestEnd));
+            return new UpdateProgress(ids.size(), 0);
+        } catch (WriteException we) {
+            log.warn("Failed to write " + iabs, we);
+            return new UpdateProgress(0, ids.size());
+        }
     }
 
     private void ensureContainers(Iterable<Content> items) throws StoreException {
@@ -124,26 +172,6 @@ public class ChannelDayScheduleBootstrapTask implements Callable<UpdateProgress>
             }
         }
         return containerIds.build();
-    }
-
-    private UpdateProgress tryWriteItem(Optional<Content> item, Broadcast broadcast, Channel channel) {
-        if (!item.isPresent()) {
-            log.warn("content not resolved for broadcast " + broadcast);
-            return UpdateProgress.FAILURE;
-        }
-        ItemAndBroadcast iab = new ItemAndBroadcast((Item) item.get(), broadcast);
-        try {
-            ScheduleHierarchy sh = ScheduleHierarchy.itemOnly(iab);
-            scheduleWriter.writeSchedule(ImmutableList.of(sh), channel, intervalOf(broadcast));
-            return UpdateProgress.SUCCESS;
-        } catch (WriteException we) {
-            log.warn("Failed to write " + iab, we);
-            return UpdateProgress.FAILURE;
-        }
-    }
-
-    private Interval intervalOf(Broadcast broadcast) {
-        return new Interval(broadcast.getTransmissionTime(), broadcast.getTransmissionEndTime());
     }
 
     private OptionalMap<Id, Content> resolveItems(ChannelSchedule channelSchedule)
